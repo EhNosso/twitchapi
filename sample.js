a526f5cd5e5f262ba705d286a729e0d1c25e04b6
Andre <andre@tos.world>
2022-08-01 02:04:10 -0300

    change repo

--- sample.js
+++ sample.js
@@ -2,15 +2,15 @@ const TwitchApi = require('./index.js');
 const twitchApi = new TwitchApi('client_id', 'client_secret');
 
 (async () => {
-    const broadcaster = await twitchApi.getBroadcaster('ratoborrachudo');
+    const broadcaster = await twitchApi.getBroadcaster('paulinholokobr');
     console.log("broadcaster", broadcaster);
 
-    const channel_info = await twitchApi.getChannelInfo(broadcaster.id);
-    console.log("channel_info", channel_info);
+    //const channel_info = await twitchApi.getChannelInfo(broadcaster.id);
+    //console.log("channel_info", channel_info);
 
     const streams = await twitchApi.getStreamsStatus(broadcaster.id);
     console.log("streams", streams);
 
-    //const videos = await twitchApi.getVideos(broadcaster.id);
+    //onst videos = await twitchApi.getVideos(broadcaster.id);
     //console.log("videos", videos);
 })();
